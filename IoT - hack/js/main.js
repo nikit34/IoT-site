ebc6122be294bf0447d0c0d4995a8211125060de
Никита Пермяков <38742537+nikit34@users.noreply.github.com>
2019-05-04 12:16:34 +0500

    Add files via upload

---
+++ IoT - hack/js/main.js
@@ -0,0 +1,9 @@
+$(document).ready(function() {
+  $('.header').height($(window).height());
+})
+
+$(document).ready(function() {
+  $('.nivo-lightbox a').nivoLightbox({
+    effect: 'fade'
+  });
+});
